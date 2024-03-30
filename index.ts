b44c0d16231c5a565727a8d1518d9e04adc3226d
kiranwahid <160255001+kiranwahid@users.noreply.github.com>
2024-03-30 09:59:08 -0700

    Number-guessing-game

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+// ========== Generate randome number ============
+const randomNumber = Math.floor(Math.random()*10 + 1);
+
+//  ============Taking input from user through inquirer ===========
+const userinput = await inquirer.prompt([
+    {
+        message: "Please guess a secrect number between 1 to 10.",
+        type: "number",
+        name: "guessedNumber",
+    },
+]);
+
+
+// ============== conditional Statment ==============
+
+console.log(`Guessed No is ${userinput.guessedNumber} Correct Answer is ${randomNumber}`)
+if(userinput.guessedNumber === randomNumber){
+    console.log(`congratulation! you guessed a correct number`);
+}else{
+   console.log(`ops! try again please`);
+}
